e3da1da954f2e62544a468686fce1b603afdf599
Suresh Nagulapalli <sureshnagulapalli13@gmail.com>
2020-08-14 11:14:16 -0500

    add auth

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
 const mongoose = require('mongoose');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(isAuth);
 
 app.use(
   '/graphql',
@@ -21,11 +24,9 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.Mongo_User}:${
-      process.env.Mongo_Password
-  }@cluster0-n9a0x.mongodb.net/${process.env.Mongo_DB}?retryWrites=true&w=majority`
-  )
+    `mongodb+srv://graphql:suresh-graphql@cluster0-n9a0x.mongodb.net/events-react-dev?retryWrites=true&w=majority`)
   .then(() => {
+    console.log("server started");
     app.listen(3000);
   })
   .catch(err => {
